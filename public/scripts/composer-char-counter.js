380c841a3b180b8c67ce30b5730f06c63a6cc537
sophia ojegba <roseysofia@yahoo.com>
2023-08-08 11:24:55 -0400

    Added a nav header and layout css format

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $("#tweet-text").on("input", function (){
+    const count = $(this).siblings().find(".counter")
+    let inputCount = 140 - $("#tweet-text").val().length;
+    count.text(inputCount);
+
+    if (inputCount >= 0) {
+      count.css('color', 'black');
+    } else {
+      count.css('color', 'red');
+    }  
+
+  });
+});
