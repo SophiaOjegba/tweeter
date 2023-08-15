6ccd13533d27f4eafd7dda29b2e90e3fbdb939d0
sophia ojegba <roseysofia@yahoo.com>
2023-08-14 21:43:01 -0400

    Add newest tweets on top, comment code and clear error when valid

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,22 +1,25 @@
-$(() => {
+$(()=>{
+  // Initial setup: hide error messages and the new tweet form
   $('.error-message').hide();
   $('.new-tweet').hide();
 
-  const escape = function(str) {
+   // Helper function to escape HTML in strings
+  const escape = function (str) {
     let div = $("<div></div>").text(str);
     return div.html();
   };
-
+  
+    // Function to render tweets on the page
   const renderTweets = function(tweets) {
     $('.tweet-container').empty()
-    for (let item of tweets) {
-      const result = createTweetElement(item)
-      $('.tweet-container').append(result)
+    for (let item of tweets) {      
+      const result = createTweetElement(item) 
+      $('.tweet-container').append(result)  
     }
   }
-
+    // Function to create HTML for a single tweet
   const createTweetElement = function(tweet) {
-    let $tweet = `
+  let $tweet = `
     <article class="tweetArticle">
     <header class="tweet-header">
       <div class="profile-picture">
@@ -38,68 +41,91 @@ $(() => {
     </footer>
     </article> <br>
   `
-    return $tweet;
+  return $tweet;
   }
 
+  // Handling compose tweet button click
   const composeTweet = $('.nav-button')
-  composeTweet.on('click', function() {
+  composeTweet.on('click', function () {
     if ($('.new-tweet').is(":hidden")) {
-      $('.new-tweet').slideDown("slow");
+      $('.new-tweet').slideDown( "slow" );
       $('form').slideup('slow');
     }
   })
 
+   // Handling form submission
   const form = $('form');
-  form.on("submit", function(event) {
-    event.preventDefault();
-    console.log('form submission!');
+  form.on("submit", function (event)  {
+  event.preventDefault();
+  console.log('form submission!');
+  
+  // Display error message if tweet is empty or too long
+  let validTweet = $("#tweet-text").val().length;
+  if (validTweet === 0) {
+    $(".error-message").html(
+      `<p><i class="fa-solid fa-triangle-exclamation"></i>Your tweet can not be empty<i class="fa-solid fa-triangle-exclamation"></i></p>`
+    );
+    $('.error-message').slideDown( "slow" );
+    return false;
+  }
 
-    let validTweet = $("#tweet-text").val().length;
+  if ( validTweet > 140) {
+    $(".error-message").html(
+      `<p><i class="fa-solid fa-triangle-exclamation"></i>Your tweet is too long <i class="fa-solid fa-triangle-exclamation"></i></p>`
+    );
+    $('.error-message').slideDown( "slow" );
+    return false;
+  }
 
-    if (validTweet === 0) {
-      $(".error-message").html(
-        `<p><i class="fa-solid fa-triangle-exclamation"></i>Your tweet can not be empty<i class="fa-solid fa-triangle-exclamation"></i></p>`
-      );
-      $('.error-message').slideDown("slow");
-      return false;
-    }
+   // Clear the error if valid
+  $(".error-message").html("");
+  $('.error-message').slideUp("slow"); 
 
-    if (validTweet > 140) {
-      $(".error-message").html(
-        `<p><i class="fa-solid fa-triangle-exclamation"></i>Your tweet is too long <i class="fa-solid fa-triangle-exclamation"></i></p>`
-      );
-      $('.error-message').slideDown("slow");
-      return false;
-    }
-
-    const data = $(this).serialize();
+  // Submit the tweet data
+  const data =$( this ).serialize();
 
     $.ajax({
-        type: "POST",
-        url: '/tweets',
-        data
-      })
-      .then(() => {
-        loadTweets()
-      });
+      type: "POST",
+      url: '/tweets',
+      data
+    })
+    .then(() => {
+      loadTweets()
+    })
+    .fail(() => {
+      $(".error-message").html(
+        `<p><i class="fa-solid fa-triangle-exclamation"></i>Error submitting tweet<i class="fa-solid fa-triangle-exclamation"></i></p>`
+      );
+      $('.error-message').slideDown("slow");
+    });
   });
 
-  const loadTweets = function() {
+  // Function to load tweets from the server
+  const loadTweets = function () {
     $.ajax({
-        method: "GET",
-        dataType: "json",
-        url: 'http://localhost:8080/tweets',
-      })
-      .then((tweetData) => {
-        renderTweets(tweetData)
-        $("#tweet-text").val('');
-        $('.counter').val(140);
-        $(".tweetArticle").append(tweetData);
-      })
+      method: "GET",
+      dataType: "json",
+      url: '/tweets',
+    })
+    .then((tweetData) => {
+      // Reverse the tweetData array to display newest tweets first
+      const reversedTweetData = tweetData.reverse();
+  
+      renderTweets(reversedTweetData);
+      $("#tweet-text").val('');
+      $('.counter').val(140);
+    })
+    .fail(() => {
+      $(".error-message").html(
+        `<p><i class="fa-solid fa-triangle-exclamation"></i>Error submitting tweet<i class="fa-solid fa-triangle-exclamation"></i></p>`
+      );
+      $('.error-message').slideDown("slow");
+    });
   }
   loadTweets();
-
-  const formatDate = function(timeStamp) {
+  
+  // Helper function to format timestamps using timeago library
+  const formatDate = function (timeStamp) {
     return timeago.format(timeStamp);
   }
 
